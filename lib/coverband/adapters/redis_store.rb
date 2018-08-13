0875052788116fcad1f0f09c19fbc6a1b89b15f4
Viktor Silakov <viktor.silakov@activeplatform.com>
2018-08-13 16:16:13 +0300

    optimize redis adapter

--- lib/coverband/adapters/redis_store.rb
+++ lib/coverband/adapters/redis_store.rb
@@ -3,6 +3,7 @@
 module Coverband
   module Adapters
     class RedisStore < Base
+      attr_accessor :existing_coverage
       BASE_KEY = 'coverband2'
 
       def initialize(redis, opts = {})
@@ -24,7 +25,28 @@ module Coverband
         Coverband.configuration.logger.info(msg)
       end
 
+      # def save_report(report)
+      #   log '@@@array'
+      #   log Benchmark.measure {
+      #     store_array(base_key, report.keys)
+      #   }
+      #
+      #   maps = []
+      #   log '@@@each'
+      #   log Benchmark.measure {
+      #     report.each do |file, lines|
+      #       maps << create_map("#{base_key}.#{file}", lines)
+      #     end
+      #   }
+      #
+      #   log '@@@store'
+      #   log Benchmark.measure {
+      #     store_maps(maps)
+      #   }
+      # end
+
       def save_report(report)
+        @existing_coverage = build_existing_coverage(report.keys)
         log '@@@array'
         log Benchmark.measure {
           store_array(base_key, report.keys)
@@ -34,11 +56,6 @@ module Coverband
         log '@@@each'
         log Benchmark.measure {
           report.each do |file, lines|
-            # store_map("#{base_key}.#{file}", lines)
-            #if create_map("#{base_key}.#{file}", lines).nil?
-            #  Coverband.configuration.logger.info("FFF:::::: #{file}")
-            #  Coverband.configuration.logger.info("LLL:::::: #{lines}")
-            #end
             maps << create_map("#{base_key}.#{file}", lines)
           end
         }
@@ -69,44 +86,59 @@ module Coverband
 
       attr_reader :redis
 
-      def store_map(key, values)
-        unless values.empty?
-          existing = redis.hgetall(key)
-          # in redis all keys are strings
-          values = Hash[values.map { |k, val| [k.to_s, val] }]
-          values.merge!(existing) { |_k, old_v, new_v| old_v.to_i + new_v.to_i }
-          # Coverband.configuration.logger.info("KEY:::::: #{key}")
-          # Coverband.configuration.logger.info("VALUES:::::: #{values}")
-          redis.mapped_hmset(key, values)
-          redis.expire(key, @ttl) if @ttl
+      def build_existing_coverage(keys)
+        out = {}
+        keys.each do |key|
+          out["#{base_key}.#{key}"] = redis.hgetall("#{base_key}.#{key}")
         end
+        out
       end
+
+      # def store_map(key, values)
+      #   unless values.empty?
+      #     existing = redis.hgetall(key)
+      #     # in redis all keys are strings
+      #     values = Hash[values.map { |k, val| [k.to_s, val] }]
+      #     values.merge!(existing) { |_k, old_v, new_v| old_v.to_i + new_v.to_i }
+      #     redis.mapped_hmset(key, values)
+      #     redis.expire(key, @ttl) if @ttl
+      #   end
+      # end
 
       def create_map(key, values)
         unless values.empty?
-          existing = redis.hgetall(key)
-          # in redis all keys are strings
           values = Hash[values.map { |k, val| [k.to_s, val] }]
+
+          existing = existing_coverage[key]
           values.merge!(existing) { |_k, old_v, new_v| old_v.to_i + new_v.to_i }
-          # Coverband.configuration.logger.info("KEY:::::: #{key}")
-          # Coverband.configuration.logger.info("VALUES:::::: #{values}")
         end
-        { key => values }
+        ({ key => values })
       end
 
       def store_maps(maps)
-        # Coverband.configuration.logger.info("MAPS:::::: #{maps}")
-        redis.pipelined do
+        redis.multi do |multi_redis|
           maps.each do |hash|
-            key = hash.keys[0]
-            values = hash[hash.keys[0]]
+            key = hash.keys.first
+            values = hash[key]
             next if values.empty?
-            redis.mapped_hmset(key, values)
-            redis.expire(key, @ttl) if @ttl
+            multi_redis.mapped_hmset(key, values)
+            multi_redis.expire(key, @ttl) if @ttl
           end
         end
       end
 
+      # def store_maps(maps)
+      #   # Coverband.configuration.logger.info("MAPS:::::: #{maps}")
+      #   redis.pipelined do
+      #     maps.each do |hash|
+      #       key = hash.keys[0]
+      #       values = hash[hash.keys[0]]
+      #       next if values.empty?
+      #       redis.mapped_hmset(key, values)
+      #       redis.expire(key, @ttl) if @ttl
+      #     end
+      #   end
+      # end
 
       def store_array(key, values)
         redis.sadd(key, values) unless values.empty?
