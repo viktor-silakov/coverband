1af468eabd6a132d0a6b33a23d680d0e1775e5bc
Viktor Silakov <viktor.silakov@activeplatform.com>
2018-08-17 08:54:21 +0300

    load additional files before environment

--- lib/coverband/reporters/simple_cov_report.rb
+++ lib/coverband/reporters/simple_cov_report.rb
@@ -9,7 +9,8 @@ module Coverband
           mem.merge(
               { file => (
               scov = SimpleCov::LinesClassifier.new.classify(File.foreach(file))
-              cover_hits.map.with_index { |cover_hits, ind| (cover_hits.to_i + scov[ind]) unless scov[ind].nil? }
+              # cover_hits.map.with_index { |cover_hits, ind| (cover_hits.to_i + scov[ind]) unless scov[ind].nil? }
+              cover_hits.map.with_index { |cover_hits, ind|  cover_hits.to_i + scov[ind]  unless scov[ind].nil? }
               ) }
           )
         end
